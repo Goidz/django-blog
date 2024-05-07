32e3c4680ffa81740aece4094e396affa3b04126
Gideon Horn <gideon.h99@gmail.com>
2024-05-07 10:33:18 +0000

    typo

--- codestar/settings.py
+++ codestar/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get(" 4Ggdcp25SV4w6A/Kgeqcq4KZWWCSfTeO")
+SECRET_KEY = os.environ.get("4Ggdcp25SV4w6A/Kgeqcq4KZWWCSfTeO")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
