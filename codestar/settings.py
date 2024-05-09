50a9526541c1597d3be28507af7f1ca775beb7d6
Gideon Horn <gideon.h99@gmail.com>
2024-05-09 08:24:14 +0000

    Create Comment model

--- codestar/settings.py
+++ codestar/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ["8000-goidz-djangoblog-jh2mkqovggi.ws-eu111.gitpod.io", '.herokuapp.com']
 
